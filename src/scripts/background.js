52a73e6955dcec7a16e7f8f51ef3791656dcd022
Seth Falco <seth@falco.fun>
2025-04-22 10:15:39 +0100

    chore: update copyright notices

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2023 Elypia and Contributors
+ * Copyright 2022-2025 Seth Falco and r/Redirect Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
